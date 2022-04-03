9eb57d029631cf58d22a9b1cc84799abd2d4cf5b
Andres Cordova <andrescordovaoficial@gmail.com>
2022-04-02 20:53:59 -0600

    Class 02/04/2021

--- react-js/my-app/src/App.js
+++ react-js/my-app/src/App.js
@@ -1,14 +1,37 @@
+import React from 'react';
 import './App.css';
 
-function Helloworld(){
-  return(<div>Hello World</div>);
+// function Helloworld(props){
+//   return(<div>Hello World, {props.mytext}</div>);
+// }
+
+class Helloworld extends React.Component{
+
+  state = {
+    show: true
+  }
+
+  isLiked = 0;
+
+  render(){
+    if(this.state.show){
+      return(
+        <div>Hello World, {this.props.mytext} <button onClick={()=>{alert("you has touched this button")}}>Toggle Show</button></div>
+      )
+    }
+    else{
+      return(
+        <div>There's nothing to show you!</div>
+      )
+    }
+  }
 }
 
 // const App = () => <div>This is my component: <Helloworld/> </div>;
 
 function App() {
   return (
-    <div>This is my component: <Helloworld/> </div>
+    <div>This is my component: <Helloworld mytext="This is Andres!"/> <Helloworld/> <Helloworld/> </div>
   );
 }
 
