bff26395d849e35c4d90f631b60e58a13c9e90f0
Andres Cordova <andrescordovaoficial@gmail.com>
2022-04-19 20:55:38 -0600

    Class 19/04/2021

--- react-js/my-app/src/components/Task.js
+++ react-js/my-app/src/components/Task.js
@@ -1,12 +1,32 @@
 import React, {Component} from "react";
+import './Task.css'
+import { PropTypes } from "prop-types";
 
 class Taks extends Component{
+
+    styleCompleted(){
+        return{
+            fontSize: '20px',
+            color: this.props.task.done ? 'gray' : 'black',
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
+
     render(){
-        return <div>
-            {this.props.task.title} - {this.props.task.description} - {this.props.task.done}
-            <input type='checkbox'></input><button>x</button>
+
+        const {task} = this.props;
+
+        return <div style={this.styleCompleted()}>
+            {task.title} - 
+            {task.description} - 
+            {task.done}
+            <input type='checkbox'></input><button className="closebtn">x</button>
         </div>
     }
+}
+
+Taks.propTypes = {
+    task: PropTypes.object.isRequired,
 }
 
 export default Taks;
--- react-js/my-app/src/components/TaskComponent.js
+++ react-js/my-app/src/components/TaskComponent.js
@@ -3,7 +3,7 @@ import Taks from "./Task";
 
 class TaksItem extends Component{
     render(){
-        return this.props.task.map(task => <Taks task={task}></Taks>)
+        return this.props.task.map(task => <Taks task={task} key={task.id}></Taks>)
     }
 }
 
